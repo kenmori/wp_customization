707e2f648a8b601db77d7a71bdc0e99e59850e40
kenjimorita <>
2020-02-01 21:06:15 +0900

    fix: style

---  twentynineteen-child/page.php
+++  twentynineteen-child/page.php
@@ -15,38 +15,44 @@ get_header();
 <section id="primary" class="content-area">
 <main id="main" class="site-main">
 
-    <?php
+
+<?php
 global $post;
 $args = array( 'posts_per_page' => 8 );
 $myposts = get_posts( $args );
 foreach( $myposts as $post ) {
 setup_postdata($post);
 ?>
-<a href="<?php the_permalink() ?>" class="lists">
-  <div class="item thum">
-    <?php if(has_post_thumbnail()): ?>
-    <?php the_post_thumbnail('index_thumbnail'); ?>
-    <?php endif; ?>
+<div class="flex-wrap">
+  <a href="<?php the_permalink() ?>">
+    <div class="flex">
+      <div class="item thum">
+        <?php if(has_post_thumbnail()): ?>
+          <?php the_post_thumbnail('index_thumbnail'); ?>
+          <?php endif; ?>
+        </div>
+        <div class="item content">
+          <span class="time">
+            <?php the_time('Y.m.d') ?>
+          </span>
+          <span class="title">
+            <?php the_title(); ?>
+          </span>
+          <span>
+            <?php the_content(); ?>
+          </span>
+        </div>
+      </div>
+    </a>
   </div>
-
-  <div class="item content">
-    <span class="time">
-      <?php the_time('Y.m.d') ?>
-    </span>
-    <span class="title">
-      <?php the_title(); ?>
-    </span>
-  </div>
-</a>
 <?php
 }
 wp_reset_postdata();
 ?>
-
-
 
 </main><!-- #main -->
 </section><!-- #primary -->
 
 <?php
 get_footer();
+?>
