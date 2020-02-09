89a02dfecc47e76756556c70a3966ad4f77abc9d
kenjimorita <>
2020-02-10 07:01:43 +0900

    fix: style

--- bukotsu/wp-content/themes/fourteenchild/index.php
+++ bukotsu/wp-content/themes/fourteenchild/index.php
@@ -18,7 +18,6 @@ get_header(); ?>
 
 <div id="contentwarap" class="snap-">
 	<div id="main-content" class="main-content">
-
 <!--
 <div id="opensociales">
   <a class="smedia email" href=""><span></span></a>
@@ -35,19 +34,18 @@ get_header(); ?>
 
 		<div id="primary" class="content-area">
 			<div id="content" class="site-content" role="main">
-
 			<?php
 				if ( have_posts() ) :
 					// Start the Loop.
+					$num = 0;
 					while ( have_posts() ) : the_post();
-
 						/*
 						 * Include the post format-specific template for the content. If you want to
 						 * use this in a child theme, then include a file called called content-___.php
 						 * (where ___ is the post format) and that will be used instead.
 						 */
 						get_template_part( 'content', get_post_format() );
-
+						$num += 1;
 					endwhile;
 					// Previous/next post navigation.
 					twentyfourteen_paging_nav();
